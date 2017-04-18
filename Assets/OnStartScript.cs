133780d307d24fba128ac0c39387aea070fdc6f0
Terppanen <trust552@hotmail.com>
2017-04-18 17:12:25 +0300

    First commit.

---
+++ Assets/OnStartScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStartScript : MonoBehaviour
+{
+    string[] tiedot;
+    public Transform Button;
+    public Transform Elokuvateatteri;
+
+    // Use this for initialization
+    void Start()
+    {
+        tiedot = System.IO.File.ReadAllLines("movies.txt");
+
+        for (int i = 0; i < tiedot.Length; i++) {
+            //    Debug.Log(tiedot[i]);
+            Instantiate(Button);
+        }
+    }
+}
---
+++ Assets/Scripts/DropdownValueChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DropdownValueChange : MonoBehaviour
+{
+
+    public Dropdown dropdown;
+    public Canvas view1;
+    public Canvas view2;
+
+    public void Start()
+    {
+        dropdown.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
+    }
+
+    public void ValueChangeCheck()
+    {
+        if (dropdown.value == 0)
+        {
+            view1.enabled = true;
+            view2.enabled = false;
+            
+        }
+        else
+        {
+            view1.enabled = false;
+            view2.enabled = true;
+        }
+    }
+}
